44dc47cdf29e5f3c50449422fe2fdbefd89d5b27
David Stinnett <davidstinnett@icloud.com>
2022-04-04 15:24:32 -0400

    completed exercise 5

---
+++ Javascript/exerciseKeylessCarTwo/exercise5/exercise5.js
@@ -0,0 +1,31 @@
+// declaring function as variable
+
+var checkDriverAge = function () {
+  var age = prompt("What is your age?");
+
+  if (Number(age) < 18) {
+    alert("Sorry, you are too yound to drive this car. Powering off");
+  } else if (Number(age) > 18) {
+    alert("Powering On. Enjoy the ride!");
+  } else if (Number(age) === 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+  }
+};
+
+checkDriverAge();
+
+// declaring as function
+
+function checkDriverAge2() {
+  var age = prompt("What is your age?");
+
+  if (Number(age) < 18) {
+    alert("Sorry, you are too yound to drive this car. Powering off");
+  } else if (Number(age) > 18) {
+    alert("Powering On. Enjoy the ride!");
+  } else if (Number(age) === 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+  }
+}
+
+checkDriverAge2();
