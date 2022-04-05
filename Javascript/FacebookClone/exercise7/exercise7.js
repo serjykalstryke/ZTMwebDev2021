6e402a477c0b3ddc3646435c39bdb8159d4bd60e
David Stinnett <davidstinnett@icloud.com>
2022-04-05 09:02:16 -0400

    started facebook project

---
+++ Javascript/FacebookClone/exercise7/exercise7.js
@@ -0,0 +1,21 @@
+var database = [
+  {
+    username: david,
+    password: 12345,
+  },
+];
+
+var newsfeed = [
+  {
+    username: david,
+    password: 12345,
+  },
+  {
+    username: Jane,
+    password: 12345,
+  },
+  {
+    username: Juan,
+    password: 12345,
+  },
+];
